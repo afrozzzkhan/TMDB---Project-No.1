69ce08feaa6404bff75be5b26a616de161d72c16
Afroze Pathan <afrozkhan.uak@gmail.com>
2024-08-29 01:32:04 +0530

    Changed some CSS

--- src/components/DetailsBanner/DetailsBanner.jsx
+++ src/components/DetailsBanner/DetailsBanner.jsx
@@ -1,6 +1,8 @@
 import './DetailsBanner.css'
 function DetailsBanner ({details}) {
 
+    const genre=details.genres  || [];
+
     return (
 
         <div className="details-banner">
@@ -9,12 +11,22 @@ function DetailsBanner ({details}) {
 
             <img src={`https://image.tmdb.org/t/p/original/${details.poster_path}`} />
             </div>
-
             <div className='details-banner-title'>
+                {details.original_title}
+            </div>
+            <div className='details-banner-genre'>
+                {genre.map((item) => {
+                    return <>{item.name} </>;
+                 })}
+            </div>
+
+
+            <div className='details-banner-trailer'>
             {details.videos && <a href={`https://www.youtube.com/watch?v=${details.videos.results[1].key}`} target="_blank">Watch Trailer</a>}
             </div>
         </div>
     );
+
 
 }
 
